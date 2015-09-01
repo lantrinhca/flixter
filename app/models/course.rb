27318a8f7bf9382cbc84cb2d12c8d8c4f4b23ea0
Lan <lantrinh.ca@gmail.com>
2015-09-01 14:05:20 +0000

    Student Enrollment

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,14 +1,16 @@
 class Course < ActiveRecord::Base
+	mount_uploader :image, ImageUploader
+
 	belongs_to :user
 	has_many :sections
+	has_many :enrollments
+
 	# extend FriendlyId
 	# friendly_id :title
 
 	validates :title, :presence => true
 	validates :description, :presence => true
 	validates :cost, :presence => true, :numericality => { :greater_than_or_equal_to => 0 }
-
-	mount_uploader :image, ImageUploader
 
 	def to_param
 		"#{id} #{title}".parameterize
