3f4666c263a0e3b7bf18f99fa42f0b65d9f973ee
Lan <lantrinh.ca@gmail.com>
2015-09-01 14:57:28 +0000

    Authenticate lesson problem

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,30 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_lesson
+
 	def show
 
 	end
 
 	private
-	helper_method :current_lesson
 
+	helper_method :current_lesson
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
 	end
+
+	def require_authorized_for_current_lesson
+		current_course = current_lesson.section.course
+
+
+		# if current_user.enrolled_in?(current_course) != true
+		# 	redirect_to course_path(current_lesson.section.course), :alert => 'You are not registered for the course'
+		# end
+
+		if !current_lesson.section.course.user.enrolled_in?(current_course)
+			redirect_to course_path(current_course), :alert => 'You are not registered for the course'
+		end
+	end
+
+
 end
